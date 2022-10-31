07439548244f4bcc554f9532590eca2d46b39da3
destro1108 <swanandev@gmail.com>
2022-11-01 01:25:43 +0530

    feat(app): :sparkles: created demo app for Fields

--- e2e/rhf-fluentui-app/src/app/app.tsx
+++ e2e/rhf-fluentui-app/src/app/app.tsx
@@ -1,14 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
+import { PrimaryButton } from '@fluentui/react';
+import {
+  CheckboxField,
+  InputField,
+  PasswordInputField,
+} from '@raftlabs/react-hook-form-fluentui';
+import Form from 'packages/react-hook-form-fluentui/src/lib/Form';
+import { useForm } from 'react-hook-form';
 
 export function App() {
+  const form = useForm();
   return (
-    <>
-      <NxWelcome title="rhf-fluentui-app" />
-      <div />
-    </>
+    <Form formHook={form} onSubmit={(val) => console.log(val)}>
+      <InputField formHook={form} label="Input 1" name="input1" />
+      <PasswordInputField formHook={form} label="Password" name="pass1" />
+      <CheckboxField formHook={form} label="Check IT" name="checkit" />
+      <PrimaryButton type="submit" text="Submit To Console" />
+    </Form>
   );
 }
-
 export default App;
